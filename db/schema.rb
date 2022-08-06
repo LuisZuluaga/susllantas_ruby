2b9ac54d6a41b6c04fc34e8d701eb98e841157f0
Luis Zuluaga <luiszv_211@hotmail.com>
2022-08-06 12:08:28 -0500

    Change form to create events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_06_162625) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_06_163334) do
+  create_table "cars", force: :cascade do |t|
+    t.string "plate"
+    t.text "description"
+    t.integer "client_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_cars_on_client_id"
+  end
+
   create_table "clients", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -37,5 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_06_162625) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cars", "clients"
   add_foreign_key "events", "sellers"
 end
